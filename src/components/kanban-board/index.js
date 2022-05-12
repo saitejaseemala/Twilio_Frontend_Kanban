9b35604130c593fe3408d4b48044cbcb008e02ae
saitejaseemala <saitejaseemala.2424@gmail.com>
2022-05-12 13:42:57 +0530

    Kanban board

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -4,15 +4,16 @@ import "./index.css";
 export default class KanbanBoard extends Component {
   constructor() {
     super();
-    // Each task is uniquely identified by its name. 
+    // Each task is uniquely identified by its name.
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
     this.state = {
+      newTask: "",
       tasks: [
-            { name: '1', stage: 0 },
-            { name: '2', stage: 0 },
-        ]
+        { name: "1", stage: 0 },
+        { name: "2", stage: 0 },
+      ],
     };
-    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+    this.stagesNames = ["Backlog", "To Do", "Ongoing", "Done"];
   }
 
   render() {
@@ -30,40 +31,77 @@ export default class KanbanBoard extends Component {
     return (
       <div className="mt-20 layout-column justify-content-center align-items-center">
         <section className="mt-50 layout-row align-items-center justify-content-center">
-          <input id="create-task-input" type="text" className="large" placeholder="New task name" data-testid="create-task-input"/>
-          <button type="submit" className="ml-30" data-testid="create-task-button">Create task</button>
+          <input
+            id="create-task-input"
+            type="text"
+            className="large"
+            placeholder="New task name"
+            data-testid="create-task-input"
+            onChange={this.setTask}
+          />
+          <button
+            type="submit"
+            className="ml-30"
+            data-testid="create-task-button"
+            onClick={this.pushToBacklog}
+          >
+            Create task
+          </button>
         </section>
 
         <div className="mt-50 layout-row">
-            {stagesTasks.map((tasks, i) => {
-                return (
-                    <div className="card outlined ml-20 mt-0" key={`${i}`}>
-                        <div className="card-text">
-                            <h4>{this.stagesNames[i]}</h4>
-                            <ul className="styled mt-50" data-testid={`stage-${i}`}>
-                                {tasks.map((task, index) => {
-                                    return <li className="slide-up-fade-in" key={`${i}${index}`}>
-                                      <div className="li-content layout-row justify-content-between align-items-center">
-                                        <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
-                                        <div className="icons">
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
-                                            <i className="material-icons">arrow_back</i>
-                                          </button>
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
-                                            <i className="material-icons">arrow_forward</i>
-                                          </button>
-                                          <button className="icon-only danger x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-delete`}>
-                                            <i className="material-icons">delete</i>
-                                          </button>
-                                        </div>
-                                      </div>
-                                    </li>
-                                })}
-                            </ul>
-                        </div>
-                    </div>
-                )
-            })}
+          {stagesTasks.map((tasks, i) => {
+            return (
+              <div className="card outlined ml-20 mt-0" key={`${i}`}>
+                <div className="card-text">
+                  <h4>{this.stagesNames[i]}</h4>
+                  <ul className="styled mt-50" data-testid={`stage-${i}`}>
+                    {tasks.map((task, index) => {
+                      return (
+                        <li className="slide-up-fade-in" key={`${i}${index}`}>
+                          <div className="li-content layout-row justify-content-between align-items-center">
+                            <span
+                              data-testid={`${task.name
+                                .split(" ")
+                                .join("-")}-name`}
+                            >
+                              {task.name}
+                            </span>
+                            <div className="icons">
+                              <button
+                                className="icon-only x-small mx-2"
+                                data-testid={`${task.name
+                                  .split(" ")
+                                  .join("-")}-back`}
+                              >
+                                <i className="material-icons">arrow_back</i>
+                              </button>
+                              <button
+                                className="icon-only x-small mx-2"
+                                data-testid={`${task.name
+                                  .split(" ")
+                                  .join("-")}-forward`}
+                              >
+                                <i className="material-icons">arrow_forward</i>
+                              </button>
+                              <button
+                                className="icon-only danger x-small mx-2"
+                                data-testid={`${task.name
+                                  .split(" ")
+                                  .join("-")}-delete`}
+                              >
+                                <i className="material-icons">delete</i>
+                              </button>
+                            </div>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
